53ed5daa772d25faae6db4d916bb3688aaa39723
pashcash <pasha_koshkarov@bk.ru>
2024-02-15 20:49:54 +0500

    Initial

---
+++ src/test/java/com/example/SimpleWiki/SimpleWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleWiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
